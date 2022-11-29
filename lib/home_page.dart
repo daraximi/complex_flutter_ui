f809ed5c0bbf7a63a8764af9f20a00e99b9a32a2
Darasimi Oloyede <darasimioloyede@gmail.com>
2022-11-29 13:53:37 +0100

    Finished home screen UI

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'colors.dart' as color;
@@ -10,6 +12,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List info=[];
+  _initData(){
+    DefaultAssetBundle.of(context).loadString("assets/info.json").then((value){
+      info = json.decode(value);
+    });
+  }
+  
+  @override
+  void initState(){
+    super.initState();
+    _initData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -186,7 +201,7 @@ class _HomePageState extends State<HomePage> {
                     width: double.maxFinite,
                     height: 100,
                     //color: Colors.redAccent.withOpacity(0.3),
-                    margin:  EdgeInsets.only(left: 150, top: 50),
+                    margin:  EdgeInsets.only(left: 155, top: 65),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -216,6 +231,7 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
+            SizedBox(height: 10,),
             Row(
               children: [
                 Text("Area of focus",
@@ -228,26 +244,97 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
             Expanded(
-              child: ListView.builder(
-                itemCount: 4,
-                  itemBuilder: (_,i){
-                    return Row(
-                      children: [
-                        Container(
-                          height: 170,
-                          width: 200,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            image: DecorationImage(
-                              image: AssetImage(
-                                "assets/runners.jpg"
-                              )
+              child: OverflowBox(
+                maxWidth: MediaQuery.of(context).size.width,
+                child: MediaQuery.removePadding(
+                  removeTop: true,
+                  context: context,
+                  child: ListView.builder(
+                    itemCount: (info.length.toDouble()/2).toInt(),
+                      itemBuilder: (_,i){
+                      int a = 2*i;
+                      int b = 2*i +1;
+                        return Row(
+                          children: [
+                            Container(
+                              margin:EdgeInsets.only(left:30, bottom: 15, top:15),
+                              height:200,
+                              width: (MediaQuery.of(context).size.width-90)/2,
+                              padding: EdgeInsets.only(bottom: 5),
+                              decoration: BoxDecoration(
+                                color:Colors.white,
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    info[a]['img']
+                                  )
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    blurRadius: 3,
+                                    offset: Offset(5,5),
+                                    color: color.AppColor.gradientSecond.withOpacity(0.1)
+                                  ),
+                                  BoxShadow(
+                                      blurRadius: 3,
+                                      offset: Offset(-5,-5),
+                                      color: color.AppColor.gradientSecond.withOpacity(0.1)
+                                  )
+                                ]
+                              ),
+                              child: Center(
+                                child: Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Text(
+                                    info[a]["title"],
+                                    style: TextStyle(fontSize: 20,
+                                    color: color.AppColor.homePageDetail),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height:200,
+                              margin:EdgeInsets.only(left:30, bottom: 15, top: 15),
+                              width: (MediaQuery.of(context).size.width-90)/2,
+                              padding: EdgeInsets.only(bottom: 5),
+                              decoration: BoxDecoration(
+                                  color:Colors.white,
+                                  borderRadius: BorderRadius.circular(15),
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          info[b]['img']
+                                      )
+                                  ),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        blurRadius: 3,
+                                        offset: Offset(5,5),
+                                        color: color.AppColor.gradientSecond.withOpacity(0.1)
+                                    ),
+                                    BoxShadow(
+                                        blurRadius: 3,
+                                        offset: Offset(-5,-5),
+                                        color: color.AppColor.gradientSecond.withOpacity(0.1)
+                                    )
+                                  ]
+                              ),
+                              child: Center(
+                                child: Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Text(
+                                    info[b]["title"],
+                                    style: TextStyle(fontSize: 20,
+                                        color: color.AppColor.homePageDetail),
+                                  ),
+                                ),
+                              ),
                             )
-                          ),
-                        )
-                      ],
-                    );
-                  }
+                          ],
+                        );
+                      }
+                  ),
+                ),
               ),
             )
           ],
